3d48f295b2c9ea1a78cdbcd4c4f7f17b783eec95
ok <ok@example.com>
2026-04-25 20:15:49 +0500

    Sync main.py move to root repo

--- backend/start.sh
+++ backend/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # start.sh
 export PYTHONPATH=$PYTHONPATH:.
-echo "Running from: $(pwd)"
-echo "PYTHONPATH: $PYTHONPATH"
-
-# Increase timeout to 120s to allow MongoDB Atlas handshake
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:$PORT --timeout 120
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT --timeout 120
